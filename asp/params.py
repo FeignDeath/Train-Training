95cb74f3c13ef63d3cbf93c8391dfd9ebf3bd8a0
Feign Death <gord.lame@hotmail.de>
2025-05-13 10:30:43 +0200

    new params

--- asp/params.py
+++ asp/params.py
@@ -1,2 +1,4 @@
-primary=['asp/differential/0_input.lp','asp/differential/1_path.lp','asp/differential/2_output.lp']
+primary=['asp/standard/0_input.lp','asp/standard/1_path.lp','asp/standard/2_output.lp']
+primary=['asp/timestepless/0_input.lp','asp/timestepless/1_path.lp','asp/timestepless/2_output.lp']
+primary=['asp/differential/test.lp']
 secondary=[]
